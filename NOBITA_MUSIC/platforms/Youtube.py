5e4eeb72ac4b827752000729b00a4a72df744012
aditya88402 <aditya266015@gmail.com>
2025-08-30 10:53:31 +0530

    Update Youtube.py

--- NOBITA_MUSIC/platforms/Youtube.py
+++ NOBITA_MUSIC/platforms/Youtube.py
@@ -15,9 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 import base64
-from AnonXMusic import LOGGER
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from NOBITA_MUSIC import LOGGER
+from NOBITA_MUSIC.utils.database import is_on_off
+from NOBITA_MUSIC.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
@@ -677,4 +677,5 @@ class YouTubeAPI:
         
         return downloaded_file, direct
       
+
 
